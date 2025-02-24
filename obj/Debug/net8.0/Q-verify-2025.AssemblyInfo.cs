3591710090a975c615bcf07d4b3410a2f73e5930
filipnyden <132461429+filipnyden@users.noreply.github.com>
2025-02-24 13:25:22 +0100

    test fix

--- obj/Debug/net8.0/Q-verify-2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/Q-verify-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q-verify-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a26a897202f7069095b918d4f80173365b5e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q-verify-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Q-verify-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
